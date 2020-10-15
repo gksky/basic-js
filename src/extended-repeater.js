c951d3bccdfc2c8ad2899c993f7bf4858c648f39
gksky <a.gutkovsky@gmail.com>
2020-10-15 21:21:43 +0100

    feat: implement extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  add = [];
+  res = [];
+  if (str !== null && str != undefined) add[0] = str.toString(); else add[0] = 'null';
+  if (options.addition === undefined) options.addition = '';
+  if (options.addition === null) options.addition = 'null'; else options.addition = options.addition.toString();
+
+  if (options.addition) {
+    add.push(options.addition);
+    if (Number.isInteger(options.additionRepeatTimes) && options.additionRepeatTimes > 1) {
+      for (j = 1; j < options.additionRepeatTimes; j++) {
+        if (options.additionSeparator) add.push(options.additionSeparator, options.addition); else add.push('|', options.addition);
+      }
+    }
+  }
+  res = add;
+  if (Number.isInteger(options.repeatTimes) && options.repeatTimes > 0) {
+    for (i = 1; i < options.repeatTimes; i++) {
+        if (options.separator) res = res.concat(options.separator, add); else res = res.concat('+', add);
+    }
+  }
+  return res.join('');
 };
   
