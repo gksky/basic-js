6f3f26d6bc315a22cda9dd4902f4130e8dc6f31f
Alexander Gutkovsky <a.gutkovsky@gmail.com>
2020-10-28 17:03:25 +0300

    add: transform array func

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  //console.log(arr);
+  if (!Array.isArray(arr)) {
+    throw new Error('Аргумент функции не является массивом');
+  }
+
+  brr = [];
+  for (i = 0; i < arr.length; i++) {
+    if (typeof arr[i] == 'string') {
+      switch (arr[i]) {
+        case '--discard-next':
+        i = i + 2;
+          break;
+        case '--double-prev':
+          if (i > 0) brr.push(arr[i-1]);
+          break;
+        case '--discard-prev':
+          if (i > 0) brr.pop();
+          break;
+        case '--double-next':
+          i++;
+          if (i < arr.length) brr.push(arr[i], arr[i]);
+          break;
+        default:
+          brr.push(arr[i]);
+      }
+    } else brr.push(arr[i]);
+  }
+
+  return brr;
 };
